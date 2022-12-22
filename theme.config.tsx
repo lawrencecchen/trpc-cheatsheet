6045b25b2e559d6cd192ffe7a96a4a7aac82fdcd
Lawrence Chen <54008264+lawrencecchen@users.noreply.github.com>
2022-12-21 20:03:34 -0800

    stuff

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span className="nx-font-semibold">tRPC Cheatsheet</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/lawrencecchen/trpc-cheatsheet",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.com",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/lawrencecchen/trpc-cheatsheet",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "tRPC Cheatsheet",
   },
-}
+};
 
-export default config
+export default config;
